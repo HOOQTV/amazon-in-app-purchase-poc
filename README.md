99f8dbf8cbca3dd061d2294d797191991538226a
Dimpos Sitorus <dimpossitorus@gmail.com>
2019-09-03 18:43:17 +0700

    Let there be code

---
+++ README.md
@@ -0,0 +1,3 @@
+# Amazon In App Purchase
+
+This repository is proof of concept on implementing Amazon In App Purchase
---
+++ app/src/main/java/com/dimpossitorus/sample/amazoniap/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.dimpossitorus.sample.amazoniap
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
